dc1d4c9d47f0a0727e9b30b5cdca5a2ab1ffe557
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-03-06 09:32:18 +0530

    password is hashed

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,17 +1,24 @@
 const User = require("../models/user.models");
+const bcrypt = require("bcrypt");
 
 const createUser = async ( req, res )=>{
     const { firstName, lastName, email, password, level } = req.body;
     try {
-        const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-        level
+        bcrypt.hash(password, 5, function(err, hashedPassword) {
+            if( err ){
+                res.status(502).send({"msg":"something went wrong in encrypting password. Please try again later to singup."});
+            }else {
+                const newUser = new User({
+                    firstName,
+                    lastName,
+                    email,
+                    password:hashedPassword,
+                    level
+                });
+                newUser.save();
+                res.status(201).send({message:"User created successfully."});
+            }
         });
-        newUser.save();
-        res.status(201).send({message:"User created successfully."});
     } catch (error) {
         res.status(400).send({message:"Something went wrong in creating user!"});
     }
