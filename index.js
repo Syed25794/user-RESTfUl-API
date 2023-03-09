61096d3f3eeb238aa712b910bd0e7a3fb57fda27
Sayyad Gul Mohammad <syedgulmohammad25794@gmail.com>
2023-03-09 07:05:00 +0530

    connection with mongodb

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const connection = require("./config/database");
+require("dotenv").config();
+
+const { PORT } = process.env;
 
 const application = express();
 
 application.use(express.json());
 
-application.listen(8080,async()=>{
+application.listen(PORT,async()=>{
     try {
-        console.log("Server is running on the localhost port : 8080 ");
-        console.log("Database connnected successfully.");
+        console.log(`Server is running on the localhost port  : ${PORT} `);
         await connection();
+        console.log("Database connnected successfully.");
     } catch (error) {
         console.log("Something went wrong in connection with database!");
     }
-    
 });
